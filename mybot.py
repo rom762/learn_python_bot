15439f7d3e494e5efdd0b8db2a20992042982a3e
nickr <nickro@ya.ru>
2021-02-16 05:52:40 +0300

    added emoji

--- mybot.py
+++ mybot.py
@@ -1,31 +1,79 @@
 import credentials
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from pprint import pprint
+from emoji import emojize
+import glob
 import logging
-logging.basicConfig(filename='bot.log', level=logging.INFO)
+from pprint import pprint
 
+from random import choice, randint
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
 TOKEN = credentials.TOKEN
+
+CATS_DICT = ['кота', 'котика', 'котейку', 'кошака']
+
+
+def get_random_smile():
+    smile = choice(credentials.USER_EMOJI)
+    smile = emojize(smile, use_aliases=True)
+    return smile
 
 
 def greet_user(update, context):
     # update это нам передаст диспетчер там будет инфа, которую нам передал телеграм
     # context это непонятное что-то..
     print('вызван start')
-    print(1/0)
-    update.message.reply_text('Привет, пользователь!')
-    pprint(update)
+    smile = credentials.USER_EMOJI[0]
+    smile = emojize(smile, use_aliases=True)
+    update.message.reply_text(f'Привет, попроси меня показать тебе {choice(CATS_DICT)}! {smile}')
+
+def play_random_number(user_number):
+    bot_number = randint(user_number - 10, user_number + 10)
+    if bot_number > user_number:
+        message = f'Вы ввели {user_number}, у бота {bot_number}. Вы проиграли!'
+    elif bot_number == user_number:
+        message = f'Вы ввели {user_number}, у бота {bot_number}. Ничья!'
+    else:
+        message = f'Вы ввели {user_number}, у бота {bot_number}. Вы победили!'
+    return message
 
 
 def guess_number(update, context):
     print('вызван guess')
-    pass
+    if context.args:
+
+        try:
+            user_number = int(context.args[0])
+            message = play_random_number(user_number)
+
+        except (TypeError, ValueError):
+            message = 'Введите целое число'
+    else:
+        message = 'Введите число'
+
+    update.message.reply_text(message)
 
 
-def echo_text(update, config):
+def echo_text(update, context):
 
     message = update.message.text
-    print(message)
-    update.message.reply_text(message)
+    chat_id = update.effective_chat.id
+    print(chat_id, message)
+
+    for cat in CATS_DICT:
+        if cat in message:
+            send_cat(update, context)
+            break
+    else:
+        update.message.reply_text(message)
+
+
+def send_cat(update, context):
+    print('cats')
+    cat_photos_list = glob.glob('images/*')
+    cat_pic_filename = choice(cat_photos_list)
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=open(cat_pic_filename, 'rb'))
 
 
 def main():
@@ -33,6 +81,7 @@ def main():
     dp = my_bot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(CommandHandler('guess', guess_number))
+    dp.add_handler(CommandHandler('cat', send_cat))
     dp.add_handler(MessageHandler(Filters.text, echo_text))
 
 
